f6a0282452f81940512998cce202eeaf49a7c5f3
Crystal <127857810+cpriss1979@users.noreply.github.com>
2025-02-15 22:18:01 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,34 @@
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open("flab-to-fab-cache").then((cache) => {
-            return cache.addAll([
-    
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/app.js",
-  "/.nojekyll",
-  "/404.html",
-  "/calculator.html",
-  "/exercises.html",
-  "/foods.html",
-  "/foods0.html",
-  "/manifest.json",
-  "/progress.html",
-  "/script.js",
-  "/yoga.png",
-  "/ZibbyDiamonds.png",
-  "/ZibbyDiamonds0.png"
-                
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open("flab-to-fab-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/app.js",
+        "/.nojekyll",
+        "/404.html",
+        "/calculator.html",
+        "/exercises.html",
+        "/foods.html",
+        "/foods0.html",
+        "/manifest.json",
+        "/progress.html",
+        "/script.js",
+        "/yoga.png",
+        "/ZibbyDiamonds.png",
+        "/ZibbyDiamonds0.png",
+      ]).catch((err) => {
+        console.error("Failed to cache:", err);
+      });
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
